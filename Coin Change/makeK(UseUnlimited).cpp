0a8b78900939854d40ddd7506bdfb9649a1b3061
Amantu Amir <42311923+Amantu-Amir@users.noreply.github.com>
2021-02-03 02:10:51 +0600

    Create makeK(UseUnlimited).cpp

---
+++ Coin Change/makeK(UseUnlimited).cpp
@@ -0,0 +1,48 @@
+
+//Make k using unlimited
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+void yesNo(bool ck){cout<<(ck? "YES\n":"NO\n");}
+const int N=1e6;
+
+void solve(ll t){
+	ll n; cin>>n;
+	vector<ll>v(n);
+	for(ll i=0; i<n; i++){
+		cin>>v[i];
+	}
+	ll k; cin>>k;
+	vector<ll>dp(k+5,0);
+	dp[0]=1;
+	for(ll j=0; j<n; j++){
+		for(ll i=0; i<=k; i++){
+			if(i>=v[j]){				//use unlimited
+				dp[i] |= dp[i-v[j]];
+			}
+		}
+	}
+	yesNo(dp[k]);
+}
+
+int main(){
+
+	//freopen("output.txt", "w", stdout);
+	//freopen("input.txt", "r", stdin);
+	//ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+	//pre_calc();
+
+	ll t=1;
+	for(ll T=1; T<=t; T++){
+		solve(T);
+	}
+	return 0;
+}
+
