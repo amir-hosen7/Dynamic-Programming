ab9244c9a3587535fb08b0ed85baa351ec9677f8
Aman <42311923+Amantu-Amir@users.noreply.github.com>
2021-04-17 20:12:55 +0600

    Update makeKtakingMin.cpp

--- Coin Change/makeKtakingMin.cpp
+++ Coin Change/makeKtakingMin.cpp
@@ -16,7 +16,7 @@ typedef unsigned long long ull;
 //Timus: 314795JS
 
 //knight moves
-//ll X[8] = {2, 1, -1, -2, -2, -1, 1, 2}; 	
+//ll X[8] = {2, 1, -1, -2, -2, -1, 1, 2};   
 //ll Y[8] = {1, 2, 2, 1, -1, -2, -2, -1}; 
 //ll dx[]={1,-1,0,0};
 //ll dy[]={0,0,1,-1};
@@ -36,53 +36,73 @@ const ll p=29;
 const ll N=2e6+50;
 const ll mod=1e9+7;
 
+ll idx;
+ll dp[2000][2000];
+vector<ll>res[2000];
+vector<ll>coins(2000);
+
+void print_taken_coins(ll n, ll k){
+	if(n==0 or k==0){
+		idx++;
+		return;
+	}
+	ll val=dp[n][k];
+	if(dp[n-1][k]==val){
+		print_taken_coins(n-1,k);
+	}
+	if(k>=coins[n]){
+		res[idx].pb(coins[n]);
+		print_taken_coins(n,k-coins[n]);
+	}
+}
+
 int main(){
 
-	//freopen("C:/Users/Aman/Documents/output.txt", "w", stdout);
-	//freopen("C:/Users/Aman/Documents/input.txt", "r", stdin);
-	//ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //freopen("C:/Users/Aman/Documents/output.txt", "w", stdout);
+    //freopen("C:/Users/Aman/Documents/input.txt", "r", stdin);
+    //ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-	int t=1;
-	for(int T=1; T<=t; T++){
-		int n,amount; 
-        cin>>n>>amount;
-		vector<int>coins(n);
-		for(int i=0; i<n; i++){
-			cin>>coins[i];
-		}
-        int dp[n+5][amount+5];
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=amount; j++){
-                dp[i][j]=amount+1;
+    ll t=1;
+    for(ll T=1; T<=t; T++){
+        ll n,k; cin>>n>>k;
+        for(ll i=1; i<=n; i++){
+            cin>>coins[i];
+        }
+        for(ll i=0; i<=n; i++){
+            for(ll j=0; j<=k; j++){
+                dp[i][j]=inf;
             }
         }
-        for(int i=0; i<n; i++){
+        for(ll i=0; i<=n; i++){
             dp[i][0]=0;
         }
-        for(int i=0; i<n; i++){
-            for(ll j=1; j<=amount; j++){
+        for(ll i=1; i<=n; i++){
+            for(ll j=1; j<=k; j++){
                 if(j>=coins[i]){
-                    if(i>0){
-                        dp[i][j]=min(dp[i-1][j],dp[i][j-coins[i]]+1);
-                    }
-                    else{
-                        dp[i][j]=min(amount+1,dp[i][j-coins[i]]+1);
-                    }
+                    dp[i][j]=min(dp[i-1][j], dp[i][j-coins[i]]+1);
                 }
                 else{
-                    if(i>0){
-                        dp[i][j]=dp[i-1][j];
-                    }
+                    dp[i][j]=dp[i-1][j];
                 }
             }
         }
-        if(dp[n-1][amount]==amount+1){
+        if(dp[n][k]>=inf){
             cout<<"-1\n";
         }
         else{
-            cout<<dp[n-1][amount]<<"\n";
+        	ll siz=dp[n][k];
+        	cout<<siz<<"\n";
+	    	print_taken_coins(n,k);
+	    	for(ll i=0; i<idx; i++){
+	    		for(ll j=0; j<siz; j++){
+	    			cout<<res[i][j]<<" ";
+	    		}
+	    		cout<<"\n";
+	    	}
         }
-	}
-	return 0;
+    }
+    return 0;
 }
+
+
 
