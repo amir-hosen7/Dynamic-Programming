5227761387e751a9937edb1b78a398eda7130bb3
Aman <42311923+Amantu-Amir@users.noreply.github.com>
2021-04-22 02:54:06 +0600

    Create 1509C.cpp

---
+++ Basic/Codeforces/1509C.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define Mid(b,e) b+(e-b)/2
+#define inf 2147483647
+
+//---------------------------------------------------------------------------//
+//Timus: 314795JS
+
+//knight moves
+//ll X[8] = {2, 1, -1, -2, -2, -1, 1, 2}; 	
+//ll Y[8] = {1, 2, 2, 1, -1, -2, -2, -1}; 
+//ll dx[]={1,-1,0,0};
+//ll dy[]={0,0,1,-1};
+void yesNo(bool ck){cout<<(ck? "YES\n":"NO\n");}
+ll toInt(string s){stringstream ss;ss<<s;int n;ss>>n;return n;}
+string toString(ll n){stringstream ss;ss<<n;string s;ss>>s;return s;}
+string UPPER(string s){transform(all(s),s.begin(),::toupper);return s;}
+string LOWER(string s){transform(all(s),s.begin(),::tolower);return s;}
+bool isDigit(char ch){if(ch>=48 && ch<=57) return true; else return false;}
+vector<ll>removeDup(vector<ll>v){sort(all(v)); v.resize(distance(v.begin(),unique(all(v))));return v;}
+bool isVowel(char ch){ch=toupper(ch);if(ch=='A'||ch=='U'||ch=='I'||ch=='O'||ch=='E'){return true;}return false;}
+bool isCons(char ch){if(isalpha(ch) && !isVowel(ch)){return true;} return false;}
+
+//------------------------------------------------------------------------------//
+
+const ll p=29;
+const ll N=2e6+50;
+const ll mod=1e9+7;
+
+vector<ll>A;
+ll dp[2010][2010];
+
+ll solve(ll L, ll R){
+	if(L>=R){
+		return 0;
+	}
+	if(dp[L][R]!=-1){
+		return dp[L][R];
+	}
+	ll ans=A[R]-A[L];
+	ans+=min(solve(L,R-1),solve(L+1,R));
+	return dp[L][R]=ans;
+}
+
+int main(){
+
+	//freopen("C:/Users/Aman/Documents/output.txt", "w", stdout);
+	//freopen("C:/Users/Aman/Documents/input.txt", "r", stdin);
+	//ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+	ll t=1;
+	for(ll T=1; T<=t; T++){
+		memset(dp,-1,sizeof(dp));
+		ll n; cin>>n;
+		A.resize(n);
+		for(ll i=0; i<n; i++){
+			cin>>A[i];
+		}
+		sort(all(A));
+		ll res=(A[n-1]-A[0])+min(solve(0,n-2),solve(1,n-1));
+		cout<<res<<"\n";
+	}
+	return 0;
+}
+
