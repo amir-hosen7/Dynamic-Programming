0e6606cb5b0a71b4f703f9380099a339e2a8811d
Aman <42311923+Amantu-Amir@users.noreply.github.com>
2021-04-28 00:45:32 +0600

    Create 1225. Flags.cpp

---
+++ Basic/Timus/1225. Flags.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define Mid(b,e) b+(e-b)/2
+#define inf 2147483647
+
+//---------------------------------------------------------------------------//
+//Timus: 314795JS
+
+//knight moves
+//ll X[8] = {2, 1, -1, -2, -2, -1, 1, 2}; 	
+//ll Y[8] = {1, 2, 2, 1, -1, -2, -2, -1}; 
+//ll dx[]={1,-1,0,0};
+//ll dy[]={0,0,1,-1};
+void yesNo(bool ck){cout<<(ck? "YES\n":"NO\n");}
+ll toInt(string s){stringstream ss;ss<<s;int n;ss>>n;return n;}
+string toString(ll n){stringstream ss;ss<<n;string s;ss>>s;return s;}
+string UPPER(string s){transform(all(s),s.begin(),::toupper);return s;}
+string LOWER(string s){transform(all(s),s.begin(),::tolower);return s;}
+bool isDigit(char ch){if(ch>=48 && ch<=57) return true; else return false;}
+vector<ll>removeDup(vector<ll>v){sort(all(v)); v.resize(distance(v.begin(),unique(all(v))));return v;}
+bool isVowel(char ch){ch=toupper(ch);if(ch=='A'||ch=='U'||ch=='I'||ch=='O'||ch=='E'){return true;}return false;}
+bool isCons(char ch){if(isalpha(ch) && !isVowel(ch)){return true;} return false;}
+
+//------------------------------------------------------------------------------//
+
+const ll p=29;
+const ll N=2e6+50;
+const ll mod=1e9+7;
+
+
+int main(){
+
+	//freopen("C:/Users/Aman/Documents/output.txt", "w", stdout);
+	//freopen("C:/Users/Aman/Documents/input.txt", "r", stdin);
+	//ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+	//1=Red, 2=White, 3=Red+Blue, 4=White+Blue
+
+	ll dp[50][5];
+	dp[1][1]=1; dp[1][2]=1;
+	dp[1][3]=0; dp[1][4]=0;
+	for(int i=2; i<=45; i++){
+		dp[i][1]=dp[i-1][2]+dp[i-1][4];
+		dp[i][2]=dp[i-1][1]+dp[i-1][3];
+		dp[i][3]=dp[i-1][1];
+		dp[i][4]=dp[i-1][2];
+	}
+
+	ll t=1;
+	for(ll T=1; T<=t; T++){
+		ll n; cin>>n;
+		cout<<dp[n][1]+dp[n][2]<<"\n";
+	}
+	return 0;
+}
+
