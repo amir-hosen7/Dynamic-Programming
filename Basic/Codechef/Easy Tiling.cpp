9b530f3310f3c0b43a0be5dd8eb45a32e5433465
Aman <42311923+Amantu-Amir@users.noreply.github.com>
2021-04-30 03:32:01 +0600

    Create Easy Tiling.cpp

---
+++ Basic/Codechef/Easy Tiling.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define Mid(b,e) b+(e-b)/2
+#define inf 2147483647
+
+//---------------------------------------------------------------------------//
+//Timus: 314795JS
+
+//knight moves
+//ll X[8] = {2, 1, -1, -2, -2, -1, 1, 2}; 	
+//ll Y[8] = {1, 2, 2, 1, -1, -2, -2, -1}; 
+//ll dx[]={1,-1,0,0};
+//ll dy[]={0,0,1,-1};
+void yesNo(bool ck){cout<<(ck? "YES\n":"NO\n");}
+ll toInt(string s){stringstream ss;ss<<s;int n;ss>>n;return n;}
+string toString(ll n){stringstream ss;ss<<n;string s;ss>>s;return s;}
+string UPPER(string s){transform(all(s),s.begin(),::toupper);return s;}
+string LOWER(string s){transform(all(s),s.begin(),::tolower);return s;}
+bool isDigit(char ch){if(ch>=48 && ch<=57) return true; else return false;}
+vector<ll>removeDup(vector<ll>v){sort(all(v)); v.resize(distance(v.begin(),unique(all(v))));return v;}
+bool isVowel(char ch){ch=toupper(ch);if(ch=='A'||ch=='U'||ch=='I'||ch=='O'||ch=='E'){return true;}return false;}
+bool isCons(char ch){if(isalpha(ch) && !isVowel(ch)){return true;} return false;}
+
+//------------------------------------------------------------------------------//
+
+const ll p=29;
+const ll N=2e6+50;
+const ll mod=1e9+7;
+
+map<ll,ll>F;
+ll solve(ll n){
+	if(F.count(n)){return F[n];}
+	ll k=n/2;
+	if(n%2==0){
+		return F[n]=((solve(k)*solve(k))+(solve(k-1)*solve(k-1)))%mod;
+	}
+	else{
+		return F[n]=((solve(k)*solve(k+1))+(solve(k)*solve(k-1)))%mod;
+	}
+}
+
+int main(){
+
+	//freopen("C:/Users/Aman/Documents/output.txt", "w", stdout);
+	//freopen("C:/Users/Aman/Documents/input.txt", "r", stdin);
+	//ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+	F[0]=1; F[1]=1; F[2]=2;
+	ll t=1; cin>>t;
+	for(ll T=1; T<=t; T++){
+		ll n; cin>>n;
+		cout<<solve(n)<<"\n";
+	}
+	return 0;
+}
+
